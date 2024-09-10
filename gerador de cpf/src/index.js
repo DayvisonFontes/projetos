971f6b93236d55fe118fb2e5b96dbe7e59478344
Dayvison Fontes <dayvisonfontes24@gmail.com>
2024-09-10 14:01:49 -0300

    .

---
+++ gerador de cpf/src/index.js
@@ -0,0 +1,18 @@
+import GeraCPF from './modules/module-cpf/GeraCPF'
+import './assets/css/modelo.css';
+import { cpf } from 'cpf-cnpj-validator';
+
+const btn = document.querySelector('.btn');
+const btnLimpar = document.querySelector('.limpar');
+const cpfGerado = document.querySelector('.cpf-gerado');
+
+btn.addEventListener('click', function() {
+    const gera = new GeraCPF();
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+    cpfGerado.classList.add('com-borda');
+});
+
+btnLimpar.addEventListener('click', function() {
+    cpfGerado.innerHTML = '';
+    cpfGerado.classList.remove('com-borda');
+})
